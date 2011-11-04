66f6a8df26de17dd83d05209a7c831ef4b73a170
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2011-11-05 00:36:30 +1100

    Providers and zfproject.xml

---
+++ providers/PubProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Zend/Tool/Project/Provider/Abstract.php';
+require_once 'Zend/Tool/Project/Provider/Exception.php';
+
+class PubProvider extends Zend_Tool_Project_Provider_Abstract
+{
+
+    public function parse()
+    {
+        /** @todo Implementation */
+    }
+
+
+}
+
