182e7ad655bf66f43bc46987a4fd0ac90aa292f9
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2011-11-19 22:10:06 +1100

    Importing the data is next step.

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -2,5 +2,10 @@
 class AdminController extends Zend_Controller_Action {
 	public function hancockAction() {
 		
+		
+		$file = fopen('/home/jiangti/530-project.csv', 'r');
+		while ($row = fgetcsv($file)) {
+			
+		}
 	}
 }
