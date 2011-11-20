a5133c1b1a08de9d93481abc8d3c1b8532d8a04f
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2011-11-20 21:27:47 +1100

    Mo Money Mo Problem.

--- env.php
+++ env.php
@@ -1,12 +1,7 @@
 <?php
-// Define path to application directory
 defined('APPLICATION_ROOT') || define('APPLICATION_ROOT', realpath(dirname(__FILE__)));
-// Define path to application directory
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', APPLICATION_ROOT . '/application');
-// Define application environment
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
-// Ensure library/ is on include_path
-// Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
     realpath(APPLICATION_PATH . '/models'),
