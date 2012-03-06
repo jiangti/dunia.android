069073365a22b1b1d5b3afd6e5ec838a86a5b48e
Victor Garcia <victorgarciagonzalez@gmail.com>
2012-03-06 23:13:16 +1100

    adding some experimental crawling for new pubs based on foursquare search

--- application/controllers/FoursquareController.php
+++ application/controllers/FoursquareController.php
@@ -70,4 +70,51 @@ class FoursquareController extends Model_Controller_Action {
 	    //$this->view->pubs = $foursquarePubs;
 	    exit;
 	}
+	
+	public function crawlAction() {
+	    $user = $this->_getUser();
+	    
+        $latitude  = $this->_getParam('latitude', $user->getLat());
+        $longitude = $this->_getParam('longitude', $user->getLong());
+        
+        $pubs = $this->foursquare->get('/venues/search', array(
+            'radius'	 => 1000,
+            'limit'	     => 50,
+        	'categoryId' => Aw_Service_Foursquare::CATEGORY_PUB . ',' . Aw_Service_Foursquare::CATEGORY_BAR, 
+        	'll'         => $latitude . ',' . $longitude));
+        
+        $db = Zend_Db_Table::getDefaultAdapter();
+        
+        foreach ($pubs->response->groups[0]->items as $pub) {
+            try {
+                $db->query("insert into discover (id, name, category, latitude, longitude, json) VALUES ('" . $pub->id . "', '" . $pub->name . "', '" . $pub->categories[0]->name . "', '" . $pub->location->lat . "', '" . $pub->location->lng . "', '" . json_encode($pub) . "')");
+            } catch (Exception $e) {
+                // Fuck it, it will be a duplicated ID exception
+            }
+        }
+        
+        $this->_forward('dirty');
+	}
+	
+	public function dirtyAction() {
+	    $user = $this->_getUser();
+	     
+	    $this->view->latitude  = $this->_getParam('latitude', $user->getLat());
+	    $this->view->longitude = $this->_getParam('longitude', $user->getLong());
+	    
+	    $db        = Zend_Db_Table::getDefaultAdapter();
+	    $pubs      = $db->fetchAll("select * from discover");
+	    $pubsArray = array();
+	    
+	    foreach ($pubs as $pub) {
+	        $pubsArray[] = array(
+				'name' 		=> array($pub['name']),
+				'lat' 		=> array($pub['latitude']),
+				'lng' 		=> array($pub['longitude']),
+				'type'		=> array('bar')
+	        );
+	    }
+	    
+	    $this->view->pubs = json_encode($pubsArray);
+	}
 }
