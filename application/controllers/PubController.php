4441360b6d81af544dbf9639b177f354bda39471
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2012-04-02 21:48:11 +1000

    Made title clicable, and moved share into pub controller.

--- application/controllers/PubController.php
+++ application/controllers/PubController.php
@@ -122,4 +122,31 @@ class PubController extends Zend_Controller_Action
         
         $this->view->form = $form;
     }
+    
+    public function shareAction() {
+    
+    	$form = new Form_Deal('deal');
+    
+    	$pub = null;
+    
+    	if ($id = $this->_request->getParam('id')) {
+    		if ($pub = Model_DbTable_Pub::retrieveById($id)) {
+    			$form->setRecord($pub);
+    		}
+    	}
+    	if ($this->_request->isPost() && $form->isValid($this->_request->getPost())) {
+    		$data = $form->getValues();
+    
+    		$form->file->receive();
+    
+    		$files = $form->file->getFileInfo();
+    		$data['files'] = $files;
+    		$service = new Service_Pub();
+    		$pub = $service->savePubFromShareArray($data, $pub);
+    
+    		$this->_redirect(sprintf('/index/share/id/%d', $pub->id));
+    	}
+    	$this->view->form = $form;
+    	$this->view->pub = $pub;
+    }
 }
