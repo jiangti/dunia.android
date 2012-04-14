2e892c1c5a8adb6f584ead21e4dee8fdf7f2bba1
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2012-04-14 22:13:42 +1000

    Attachment from gmail now added into mailShare table, then makes life easier.

---
+++ application/models/DbTable/Row/MailShare.php
@@ -0,0 +1,4 @@
+<?php
+class Model_DbTable_Row_MailShare extends Model_DbTable_Row_RowAbstract {
+
+}
--- application/services/Share/Mail.php
+++ application/services/Share/Mail.php
@@ -7,6 +7,7 @@
  *
  */
 class Service_Share_Mail extends Aw_Service_ServiceAbstract {
+	
 	public function fetch() {
 		
 		$options = array(
@@ -18,29 +19,62 @@ class Service_Share_Mail extends Aw_Service_ServiceAbstract {
 		);
 		$mail = new Zend_Mail_Storage_Imap($options);
 		
-		foreach ($mail as $message) {
+		foreach ($mail as $index => $message) {
 			foreach (new RecursiveIteratorIterator($message) as $part) {
-				
-				var_dump($message); exit; 
-				var_dump($part); 
-				var_dump(strtok($part->contentType, ';'));
-			    try {
-			        if (strtok($part->contentType, ';') == 'text/plain') {
-			           //base64_decode($data)
-			        } elseif (strtok($part->contentType, ';') == 'text/plain') {
-			        	$file = $part->filename;
-			        	$content = $part->getContent();
-			        	
-			        	
-			        }
-			    } catch (Zend_Mail_Exception $e) {
-			        // ignore
-			    }
+				$this->_storeMail($message);
+				$mail->moveMessage($index, 'Parsed'); 
 			}
 		}
 		
-		exit ('..');
+		unset($mail);
+		
 	}
 	
+	public function importDb() {
+		$table = new Model_DbTable_MailShare();
+		foreach ($this->getMessages() as $messageFilePath) {
+			$message = unserialize(file_get_contents($messageFilePath));
+			$row = $table->createRow();
+			$files = array();
+			foreach (new RecursiveIteratorIterator($message) as $part) {
+				$contentType = strtok($part->contentType, ';');
+				if (stripos($contentType, 'text') !== false) {
+					$row->subject = $mesasge->subject;
+					$row->body = $part->getContent();
+					$row->save();
+				}
+				
+				if (stripos(strtok($part->contentType, ';'), 'image') !== false) {
+					$section = explode(";", $part->contentType);
+					$data = (parse_ini_string($section[1]));
+					$ext = pathinfo($data['name'], PATHINFO_EXTENSION);
+					
+					$path = sprintf(APPLICATION_ROOT . '/public/mail/%d', $row->id);
+					
+					mkdir($path, true);
+					chmod($path, 0777);
+					
+					$filePath = sprintf($path . '/%s', $data['name']);
+					
+					file_put_contents($filePath, base64_decode($part->getContent()));
+				
+					$files[] = $filePath;
+				}
+			}
+			
+			$row->attachment = json_encode($files);
+			$row->save();
+			
+			unlink($messageFilePath);
+		}	
+	}
+	
+	protected function _storeMail(Zend_Mail_Message $message) {
+		return file_put_contents(APPLICATION_ROOT . '/var/mail/' . uniqid(), serialize($message));
+	}
+	
+	public function getMessages() {
+		return glob(APPLICATION_ROOT . '/var/mail/*');
+	}
 	
 }
