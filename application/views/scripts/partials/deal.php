059c2d719b452db4abfbd1574f3ab9151cc51e3e
Victor Garcia <victorgarciagonzalez@gmail.com>
2012-05-01 23:47:45 +1000

    adding description for deals

--- application/views/scripts/partials/deal.php
+++ application/views/scripts/partials/deal.php
@@ -1,15 +1,18 @@
 <?php foreach ($this->promos as $promo): ?>
     <li>
         <?php echo $this->currency($promo->price); ?>
-        <?php /*foreach ($promo->getLiquorTypes() as $liquor) {
+        <?php foreach ($promo->getLiquorTypes() as $liquor) {
             echo ' ' . $liquor->name;
             $size = $promo->getLiquorSizes();
             foreach ($size as $s) {
                 echo ' (' . $s['name'] . ')';
             }
-        }*/ ?>
-        <div>
+        } ?>
+        <div class="right">
             <?php echo $this->formatDate($promo->timeStart, 'H:i'); ?> - <?php echo $this->formatDate($promo->timeEnd, 'H:i'); ?>
+        </div>
+        <div>
+            <?echo $promo->description; ?>
         </div>
     </li>
 <?php endforeach; ?>
