43ffa64ec33b167c0b9f1c93c4440e83a328853c
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2012-05-10 20:46:20 +1000

    Further improvements.

--- library/Aw/ProgressBar.php
+++ library/Aw/ProgressBar.php
@@ -1,12 +1,14 @@
 <?php
-class Choc_ProgressBar extends Zend_ProgressBar
+class Aw_ProgressBar extends Zend_ProgressBar
 {
 	public function next($diff = 1, $text = null) {
-		
+
 		if ($text === null) {
 			$text = sprintf("MEM %sM", memory_get_peak_usage(1) / 1024 / 1024);
+		} else {
+			$text = sprintf("MEM %sM %s", memory_get_peak_usage(1) / 1024 / 1024, $text);
 		}
-		
+
 		parent::next($diff, $text);
 	}
 }
