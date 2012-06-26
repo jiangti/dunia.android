9551cfb4cfe8e266d02c4f91dadfaa79d889ab7f
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2012-06-26 23:17:33 +1000

    Sql fucked

--- migrations/1340716315-1340716315-the_idpub.php
+++ migrations/1340716315-1340716315-the_idpub.php
@@ -9,7 +9,7 @@ class DbSchema_1340716315 extends Akrabat_Db_Schema_AbstractChange
         $sql = "ALTER TABLE `mailShare` ADD COLUMN `idPub` INT(10) UNSIGNED NULL DEFAULT NULL  AFTER `dateProcessed` , 
   ADD CONSTRAINT `fk_mailShare_pub1`
   FOREIGN KEY (`idPub` )
-  REFERENCES `thirst_db`.`pub` (`id` )
+  REFERENCES `pub` (`id` )
   ON DELETE NO ACTION
   ON UPDATE NO ACTION
 , ADD INDEX `fk_mailShare_pub1` (`idPub` ASC) ;";
