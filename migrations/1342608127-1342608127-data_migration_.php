43044647c4b66920295621010be742faf058826d
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2012-07-18 20:43:22 +1000

    Migrations to reset the data.

---
+++ migrations/1342608127-1342608127-data_migration_.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Data migration.
+ */
+class DbSchema_1342608127 extends Akrabat_Db_Schema_AbstractChange
+{
+    public function up()
+    {
+        $sqls = "UPDATE
+		pub p
+		SET isChecked = (SELECT COUNT(1) FROM pubHasPromo php WHERE php.idPub = p.id);
+
+        UPDATE pub p SET isChecked = NULL WHERE isChecked = 0";
+        $this->_executeMultiLine($sqls);
+    }
+    
+    public function down()
+    {
+        $this->_db->query($sql);
+    }
+}
