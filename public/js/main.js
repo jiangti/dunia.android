8075e342cd728f91522a856021661abd0fae24b8
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2012-08-06 14:23:02 +1000

    Some fixups.

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,8 @@
 requirejs.config({
-    shim: {
-    	'libs/ember': {
-    		deps: ['libs/jquery']
-    	}
-    }
 });
 
 // Start the main app logic.
-requirejs(['libs/ember', 'libs/ember', 'libs/routes'], function() {
+requirejs(['libs/ember', 'libs/routes'], function() {
 	var app = new routes();
 	app.get("/$", function(req) {
 		require(['app/controllers/map/index']);
